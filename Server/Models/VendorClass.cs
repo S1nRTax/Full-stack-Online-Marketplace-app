347b6d5c2d25a6359560eb2fac5e8783ead5ac2e
S1ntax <badreddine.asaad4@gmaail.com>
2024-11-22 11:37:56 +0100

    implement role-based User authentication

--- Server/Models/VendorClass.cs
+++ Server/Models/VendorClass.cs
@@ -10,7 +10,7 @@ namespace Server.Models
         public string ShopId { get; set; } // Unique shop identifier
         public string ShopName { get; set; }
         public string ShopAddress { get; set; }
-        public string ShopLogo { get; set; } // URL or path to the logo
+        public string? ShopLogo { get; set; } // URL or path to the logo
         public int Popularity { get; set; } // Metric for popularity (e.g., number of sales)
 
         // Foreign key to link with User
